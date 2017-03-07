e817c14fd70f9779c720540d0c52133cc4d4fcf2
le gia trung <14110215@student.hcmute.edu.vn>
2017-03-07 22:51:51 +0700

    Hoàn Thành

---
+++ bootsample/src/main/java/bootsample/Application.java
@@ -0,0 +1,11 @@
+package bootsample;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
